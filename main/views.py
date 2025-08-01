607b344ff00aa2323a3f28f230dfbe80b7237e31
Kholodetskiy <sergh.yanovskiy@inbox.ru>
2025-08-01 22:58:19 +0500

    add new urls

--- main/views.py
+++ main/views.py
@@ -3,8 +3,15 @@ from django.http import HttpResponse
 
 
 def index(request) -> HttpResponse:
-    return render(request, "main/index.html")
+    context = {'title': "Home - главная",
+               'content': 'Магазин мебели HOME'
+               }
+    return render(request, "main/index.html", context)
 
 
 def about(request) -> HttpResponse:
-    return HttpResponse('About')
+    context = {'title': "Home - О нас",
+               'content': 'О нас',
+               'text_on_page': "Текст о магазине"
+               }
+    return render(request, "main/about.html", context)
